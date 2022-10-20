165e777f3a13f844d505974cf1c1b8e20b7f5217
carloscl2001 <carloscorteslora@gmail.com>
2022-10-20 20:13:09 +0200

    jjjj

--- ej1.java
+++ ej1.java
@@ -1,41 +1,57 @@
 import java.util.regex.Pattern;
+
+import javax.swing.border.MatteBorder;
+
 import java.util.regex.Matcher;
 
 public class ej1{
 
-    private static final String cadena = "AbC11111";
+    private static final String cadena = "7118";
  
     public static void main(String[] args) {
         
-        
-        //Pattern pat = Pattern.compile("abc.*");
-        //Matcher mat = pat.matcher(cadena);
-
-        /* 
-        if(mat.matches()){
-            System.out.println("Si");
-        }else {System.out.println("No");}
-        */
-
-
-        /**
-         * EJERCICIO 2
-         */
-        Pattern pat = Pattern.compile("[abc|ABC].*|");
+        /*
+        // EJERCICO 1
+        Pattern pat = Pattern.compile("abc.*");
         Matcher mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+        
+
+
+        //Ejercicio 2
+        pat = Pattern.compile("^(ABC|abc).*");
+        mat = pat.matcher(cadena);
         
         if(mat.matches()){
             System.out.println("Si");
         }else {System.out.println("No");}
 
-        /* 
-        pat = Pattern.compile("\\D");
+
+
+        //Ejercio 3
+        pat = Pattern.compile("\\D.*");
+        mat = pat.matcher(cadena);
+        
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+
+        
+        //Ejercicio 4
+        pat = Pattern.compile(".*\\D");
         mat = pat.matcher(cadena);
         
         if(mat.matches()){
             System.out.println("Si");
         }else {System.out.println("No");}
         */
+
+        Pattern pat = Pattern.compile(cadena);
+        Matcher mat = pat.matcher(cadena);
+        
 
 
     }
