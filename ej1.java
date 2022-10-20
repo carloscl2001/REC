3d43e1cda9c73feb600fcb5e4734e1680558bc43
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-10-20 23:21:07 +0200

    hh

--- ej1.java
+++ ej1.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 
 public class ej1{
 
-    private static final String cadena = "27";
+    private static final String cadena = " 23-34567";
  
     public static void main(String[] args) {
         
@@ -64,16 +64,61 @@ public class ej1{
         if(mat.matches()){
             System.out.println("Si");
         }else {System.out.println("No");}
-        */
+
 
         //Ejercicio 7
-        Pattern pat = Pattern.compile("[a-zA-Z]{5,10}");
+        pat = Pattern.compile("[a-zA-Z]{5,10}");
+        mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+        
+
+        //Ejercicio 8
+        Pattern pat = Pattern.compile("www.*\\.*.es");
         Matcher mat = pat.matcher(cadena);
 
         if(mat.matches()){
             System.out.println("Si");
         }else {System.out.println("No");}
+        
 
+        //Ejercicio9
+        pat = Pattern.compile("\\d\\d/\\d\\d/\\d\\d");
+        mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+        
+
+        //Ejercicio 10
+        pat = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+        mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+        
+
+        //Ejercicio 11
+        pat = Pattern.compile("[+]\\d{2}\\ \\d{2}\\ \\d{7}");
+        mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
+        */
+
+        //Ejercicio 12
+        //" 23-34567"
+        Pattern pat = Pattern.compile("\\s\\d{2}\\-\\d{5}");
+        Matcher mat = pat.matcher(cadena);
+
+        if(mat.matches()){
+            System.out.println("Si");
+        }else {System.out.println("No");}
 
     }
 }
