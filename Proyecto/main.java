20c45b5f6181d92b7df91e399bba900998fdd12c
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-11-28 21:10:29 +0100

    m,m

---
+++ Proyecto/main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/**
+ * Clase main del indexador
+ */
+public class main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Introduzca el directorio a los ficheros a indexar");
+        Scanner out = new Scanner(System.in);
+        String directorio = out.nextLine();
+
+
+
+
+    }
+
+    public static void preProcesamiento(){
+        for(int  i = 0; )
+    }
+
+}
